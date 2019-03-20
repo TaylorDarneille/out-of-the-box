06e1b3a6d6b890d6fe5a9f5a0bd40a26c596f5dd
TaylorDarneille <tmdarneille@dons.usfca.edu>
2019-03-20 15:18:46 -0700

    protecte new post route

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../models')
+const isLoggedIn = require('../middleware/isLoggedIn.js');
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
+	if(!req.user) {
+		res.send("oops! you're not logged in!")
+	}
     res.render('posts/new.ejs')
 })
 
 router.post('/', (req, res) => {
-	res.send(req.body)
+	// db.post.insert({
+	// 	subject: req.body.subject,
+	// 	content: req.body.content,	
+	// })
+	req.body.topics = req.body.topics.split(", ")
+	res.send(req.user)
 })
 
 module.exports = router
