934a3d23b42e23eaae1d864531e0728d7567723a
Lemon Marie Garrett <e.marie.garrett@gmail.com>
2019-03-20 21:49:06 -0700

    Revert "color fix"
    This reverts commit 698c01ce5274735c5b41e4c7513323d75225ae6f.

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,35 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../models');
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
 const isLoggedIn = require('../middleware/isLoggedIn.js');
 
 router.get('/new', (req, res) => {
-	var allSites;
-	db.site.findAll().then(sites => {
-		res.render('posts/new.ejs', { sites });
-	});
-});
+    res.render('posts/new.ejs')
+})
 
 router.post('/', (req, res) => {
-	db.post
-		.create({
-			subject: req.body.subject,
-			content: req.body.content,
-		})
-		.then(createdPost => {
-			topics = req.body.topics.split(', ');
-			topics.forEach(t => {
-				db.topic
-					.create({
-						name: t,
-					})
-					.then(createdTopic => {
-						createdTopic.addPost(createdPost);
-					});
-			});
-		});
+	db.post.create({
+		subject: req.body.subject,
+		content: req.body.content	
+	})
+	.then( createdPost => {
+		topics = req.body.topics.split(", ")
+		topics.forEach( t => {
+			db.topic.create({
+				name: t
+			})
+			.then(createdTopic => {
+				createdTopic.addPost(createdPost)
+			})
+		})	
+	})
 
-	res.send(req.body);
-});
+	res.send(req.body)
+})
 
-module.exports = router;
+module.exports = router
