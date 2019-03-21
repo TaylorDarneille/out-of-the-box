1bdcd307f7661cbf6e2edc605d9a555afb7636b5
TaylorDarneille <tmdarneille@dons.usfca.edu>
2019-03-20 17:29:26 -0700

    seed file returns array of objects firt for insertion to table

--- seed.js
+++ seed.js
@@ -1,11 +1,18 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const db = require('./models');
 
 request('https://www.doc.wa.gov/corrections/incarceration/default.htm', (error, response, body) => {
 	const $ = cheerio.load(body);
 	const ul = $('.padded-list').get(0)
 	const list = $(ul).find('li a').map((index, element)=>{
-		return $(element).text()
+		const deconstructedName = $(element).text().split(" ")
+		const rawAbbr = deconstructedName.pop()
+		return {
+			name: deconstructedName.join(" "),
+			abbreviation: rawAbbr.substring(1, rawAbbr.length-1),
+			url: $(element).attr('href')
+		}
 	}).get();
 	const sites = list.splice(1)
 	console.log(sites)
